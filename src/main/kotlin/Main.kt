bb10937acd5d6ac7e8042162c8bddb9c7a89575d
unknown <wuall@hotmail.comgit config --global user.name WalaceHenrique>
2021-11-03 15:22:48 -0300

    https://github.com/Walace-Henrique-Dev/CadastrodeConvidados.git

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,70 @@
+fun main(args: Array<String>) {
+
+    /*
+    1. Crie um projeto chamado cadastroDeConvidados
+    2. Crie um loop (pode ser usado o while) e coloque um when dentro da estrutura, para simular um menu de escolhas:]
+    - O Loop precisa ficar ativo enquanto uma variável simNao estiver como "Sim", a partir do momento que o usuário digitar "Não" ou qualquer outra coisa, o programa precisa ser finalizado.
+    - Se o usuário escolher 1, abre as instruções para adicionar um novo convidado
+    - Se o usuário escolher 2, abre as instruções para remover um convidado
+    - Se o usuário escolher 3, lista os convidados
+    3. Use a List para cadastrar cada convidado
+    4. Teste para ver se tudo funciona
+     */
+
+    var nome = mutableListOf("Walace", "Henrique", "Silva")
+    var add = 1
+    var remover = 2
+    var lista = 3
+
+
+    while(true){
+
+        println("Digite 1 se for adiciona uma pessoa a lista: ")
+        println("Digite 2 se for reover uma pessoa a lista: ")
+        println("Digite 3 deseja ver a lista de convidados: ")
+
+
+    }
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
